b5608c12df59b090910b8bec8fd49858c9adf530
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-11-01 12:56:04 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+// Our code will include eight classes; player, Score, rock, gem, shooter, falling objects, Point, & Game. 
+namespace cse210_student_csharp_Greed
+{
+    public class GreedGame
+    {
+
+    }
+    public class player
+    {
+
+    }
+    public class score
+    {
+
+    }
+    public class rock
+    {
+
+    }
+    public class gem
+    {
+
+    }
+    public class shooter
+    {
+
+    }
+    public class falling_objects
+    {
+
+    }
+
+}
