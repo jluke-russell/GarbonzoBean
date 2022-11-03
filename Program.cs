dc48064a306e226f6003c1280a2d2cfafe4730aa
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-11-03 16:47:25 -0600

    more done

--- Program.cs
+++ Program.cs
@@ -15,9 +15,11 @@ namespace cse210_student_csharp_Greed
         int ScreenWidth = 800;
         int RectangleSize = 15;
         int MovementSpeed = 4;
+        int count = 0;
+        var Objects = new List<Falling_objects>();
 
         Player player = new Player(ScreenHeight, ScreenWidth, MovementSpeed, RectangleSize);
-        Fall fall = new Fall();
+        Fall fall = new Fall(MovementSpeed, Objects, ScreenHeight, ScreenWidth, RectangleSize, count);
 
         Raylib.InitWindow(ScreenWidth, ScreenHeight, "Greed");
         Raylib.SetTargetFPS(60);
@@ -30,7 +32,7 @@ namespace cse210_student_csharp_Greed
             Raylib.DrawText("Score: ", 12, 12, 20, Color.WHITE);
             
             player.input();
-            fall.initialize();
+            fall.Step();
         
             Raylib.EndDrawing();
 
@@ -74,40 +76,125 @@ namespace cse210_student_csharp_Greed
     }
     public class Fall
     {
-        public void initialize()
+        public Fall(int MovementSpeed, List<Falling_objects> Objects, int ScreenHeight, int ScreenWidth, int RectangleSize, int count)
         {
-            int count = 0;
-            if (count == 10)
-            {
+            Speed = MovementSpeed;
+            objects = Objects; 
+            Height = ScreenHeight;
+            Width = ScreenWidth;
+            Size = RectangleSize;
+            Count = count;
+        }
+        int Speed;
+        List<Falling_objects> objects;
+        int Height;
+        int Width;
+        int Size;
+        int Count;
 
-                count = 0;
+
+        public void Step()
+        {
+            var Random = new Random();
+            int whichType = Random.Next(2);
+            int randomY = Random.Next(2, Speed-1);
+            int randomX = Random.Next(0, Width - Size);
+            var position = new Vector2(randomX, 0 - Size);
+            var objectsToRemove = new List<Falling_objects>();
+
+            if (Count == 20)
+            {
+                CreateObjects(whichType, randomY, position, Size);
+                Count = 0;
             }
             else
             {
-                count += 1;
+                Count += 1;
             }
-            //Rectangle[] =  
+
+            foreach (var obj in objects) 
+            {
+                obj.Draw();
+            }
+
+            foreach (var obj in objects) 
+            {
+                obj.Move();
+            }
+            foreach (var obj in objects)
+            {
+                if (obj.Position.Y > Height + 20)
+                {
+                    objectsToRemove.Add(obj);
+                }
+            }
+            objects = objects.Except(objectsToRemove).ToList();
         }
+        public void CreateObjects(int whichType, int randomY, Vector2 position, int Size)
+        {
+            switch (whichType) 
+                {
+                case 0:
+                    var rock = new Rock(Color.RED, Size);
+                    rock.Position = position;
+                    rock.Velocity = new Vector2(0, randomY);
+                    objects.Add(rock);
+                    break;
+                case 1:
+                    var gem = new Gem(Color.BLUE, Size / 2);
+                    gem.Position = position;
+                    gem.Velocity = new Vector2(0, randomY);
+                    objects.Add(gem);
+                    break;
+                }
+        }
+        
 
     }
-    abstract public class Falling_objects
+    public class Falling_objects
     {
+        
         public Vector2 Position { get; set; } = new Vector2(0, 0);
         public Vector2 Velocity { get; set; } = new Vector2(0, 0);
-        public Falling_objects()
-        {
 
+        virtual public void Draw() {
+        // Base game objects do not have anything to draw
         }
 
+        public void Move() 
+        {
+        Vector2 NewPosition = Position;
+        NewPosition.Y += Velocity.Y;
+        Position = NewPosition;
+        }
+    }
+    public class ColoredObject: Falling_objects 
+    {
+        public Color Color { get; set; }
 
+        public ColoredObject(Color color) { Color = color; }
     }
-    public class Rock: Falling_objects
+    public class Rock: ColoredObject
     {
-        // shape = O
+        public int Size { get; set; }
+        public Rock(Color color, int size): base(color) {
+            Size = size;
+        }
+        override public void Draw() {
+            Raylib.DrawRectangleLines((int)Position.X, (int)Position.Y, Size, Size+(Size/3), Color);
+        }
     }
-    public class Gem: Falling_objects
+    public class Gem: ColoredObject
     {
-        // shape = * 
+        public int Radius { get; set; }
+        public Gem(Color color, int radius): base(color) 
+        {
+            Radius = radius;
+        }
+        override public void Draw() 
+        {
+            Raylib.DrawCircle((int)Position.X, (int)Position.Y, Radius, Color);
+        }
     }
     public class Shooter
     {
